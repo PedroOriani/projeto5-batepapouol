0a28f48049adef06a3c584178b3c2ea18bfb7b4c
Pedro Oriani <pedro.oriani@gmail.com>
2023-04-16 19:36:23 -0300

    Puxando as mensagens antigas

--- script.js
+++ script.js
@@ -25,18 +25,19 @@ function nomeErro(resp){
     console.log(resp.response.status);
     if (resp.response.status === 400){
         alert ('Utilize outro nome, este ja esta sendo utilizado');
-        AccessServer()
     }
 }
 
-setInterval(AccessServer(),5000);
+setInterval(AccessServer(), 5000);
 //------------------------------------------------------------------------------------------------------
+
+let texts = [];
 
 // RESGATANDO AS MENSAGENS DO SERVIDOR
 
 function getDatas(){
 
-    const promise = axios.get('https://mock-api.driven.com.br/api/vm/uol/participants');
+    const promise = axios.get('https://mock-api.driven.com.br/api/vm/uol/messages');
     promise.then( getOK );
     promise.catch( getErro );
 
@@ -46,8 +47,44 @@ function getOK(resp){
     console.log(resp.status);
     console.log(resp.statusText);
     console.log('Dados requisitados com sucesso');
+    console.log(resp.data);
+    texts = resp.data;
+
+    renderTexts()
 }
 
 function getErro(resp){
     console.log(resp.response.status);
+}
+
+
+function renderTexts(){
+
+    const ulTexts = document.querySelector('.body-texts');
+    ulTexts.innerHTML = '';
+
+    for(let i = 0; i < texts.length; i++){
+
+        if (texts[i].type === 'status'){
+            ulTexts.innerHTML += `
+            <li class ="message-box-status">
+                ${texts[i].time} ${texts[i].from} ${texts[i].text}
+            </li>        
+            `; 
+        }else if (texts[i].type === 'message'){
+            ulTexts.innerHTML += `
+            <li class ="message-box">
+                ${texts[i].time} ${texts[i].from} para ${texts[i].to}: ${texts[i].text}
+            </li>        
+            `; 
+        }
+        else if (texts[i].type === 'message'){
+            ulTexts.innerHTML += `
+            <li class ="message-box-private">
+                ${texts[i].time} ${texts[i].from} reservadamente para ${texts[i].to}: ${texts[i].text}
+            </li>        
+            `; 
+        }
+           
+    }
 }
